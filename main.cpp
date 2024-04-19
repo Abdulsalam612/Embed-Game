4104bee7fef67b74b6dddcff7b7f117967184bd8
Abdulsalam612 <abdulsalam.sami01@gmail.com>
2024-04-19 02:49:37 +0100

    orginized

--- main.cpp
+++ main.cpp
@@ -1,124 +1,123 @@
 #include "mbed.h"
+#include "Bitmap.h"
+#include "Adafruit_ST7735.h"
+#include "Joystick.h"
+#include "N5110.h"
+#include "cuphead.h"
 
 
-#include "Adafruit_ST7735.h" // Include the Adafruit ST7735 library
 //Pin assignment:  (MOSI, MISO, SCK,  TCS,   DC,  RST)
 Adafruit_ST7735 tft(PA_7, PA_6, PA_5, PB_6, PC_10, PA_3);
 
-#include "Joystick.h" 
-//                  y     x
-Joystick joystick1(PC_1, PC_0);  //attach and create joystick object
-DigitalIn button(PB_0);
-
-Joystick joystick2(PC_2, PC_3);
-DigitalIn button2(PA_8);
-
-#include "N5110.h"
 //Pin assignment format:  lcd(IO, Ser_TX, Ser_RX, MOSI, SCLK, PWM)  
 N5110 lcd(PC_7, PA_9, PB_10, PB_5, PB_3, PA_10);
 
+// Joystick pins   y     x
+Joystick joystick1(PC_1, PC_0);  //attach and create joystick object
+Joystick joystick2(PC_2, PC_3);
 
-//set initial position of point to (42,24)
+DigitalIn button1(PB_0);
+DigitalIn button2(PA_8);
+
+
+void boundary(int x, int y);
+
+// Function Prototypes
+void updateCharacterPosition(Joystick& joystick);
+void updateShootingDirection(Joystick& joystick);
+
+
+//set initial position of the points to (42,24) for testing purposes
 float y_pos = 24;
 float x_pos = 42;
 float y_pos2 = 15;
 float x_pos2 = 20;
 
-void boundary(int x, int y);
-
 
 int main() {
-    // tft display
-    tft.initR(INITR_GREENTAB2); // Initialize with screen type, adjust if needed
-    tft.fillScreen(ST7735_RED); // Clear the display with black
 
-    //joystick
-    joystick1.init();
-    joystick2.init();
-    button.mode(PullUp);
-
-
-    //Nokia screen
+  //Nokia screen
     lcd.init(LPH7366_1);        //initialise for LPH7366-1 LCD (Options are LPH7366_1 and LPH7366_6)
     lcd.setContrast(0.6);      //set contrast to 55%
     lcd.setBrightness(0.6);     //set brightness to 50% (utilises the PWM)
+    joystick1.init();
+    joystick2.init();
+    button1.mode(PullUp);
 
-    int x_pos = 20; // Initial x position of the rectangle
 
 
     while (true) {
-        // tft.fillRect(x_pos, 20, 50, 50, ST7735_RED);
-
+ 
         lcd.clear();
         lcd.drawRect(0,0,84,48,FILL_TRANSPARENT);   //draws screen boarder
         lcd.drawRect(x_pos,y_pos,2,2,FILL_BLACK);   //draws point at position (x,y)
         lcd.drawCircle(x_pos2,y_pos2,3,FILL_BLACK);
 
-        if (button2 == 1) {  // Check if button is pressed (logic low if pressed, assuming pull-up)
-            lcd.drawRect(0,0,84,48,FILL_BLACK);
-        }
-        ThisThread::sleep_for(50ms);  // Simple debouncing
-
-  
-
-        if(joystick1.get_direction() == N){
-            y_pos--;
-        }else if(joystick1.get_direction() == S){
-            y_pos++;
-        }else if(joystick1.get_direction() == E){
-            x_pos++;
-        }else if(joystick1.get_direction() == W){
-            x_pos--;
-        }else if(joystick1.get_direction() == NE){
-            y_pos--;
-            x_pos++;
-        }else if(joystick1.get_direction() == NW){
-            y_pos--;
-            x_pos--;
-        }else if(joystick1.get_direction() == SE){
-            y_pos++;
-            x_pos++;
-        }else if(joystick1.get_direction() == SW){
-            y_pos++;
-            x_pos--;
-        } else {
-        }
 
 
-        if(joystick2.get_direction() == N){
-        y_pos2--;
-        }else if(joystick2.get_direction() == S){
-            y_pos2++;
-        }else if(joystick2.get_direction() == E){
-            x_pos2++;;
-        }else if(joystick2.get_direction() == W){
-            x_pos2--;
-        }else if(joystick2.get_direction() == NE){
-            y_pos2--;
-            x_pos2++;;
-        }else if(joystick2.get_direction() == NW){
-            y_pos2--;
-            x_pos2--;
-        }else if(joystick2.get_direction() == SE){
-            y_pos2++;
-            x_pos2++;
-        }else if(joystick2.get_direction() == SW){
-            y_pos2++;
-            x_pos2--;
-        } else {
-     }
+        updateCharacterPosition(joystick1);
+        updateShootingDirection(joystick2);
 
-
-
-
- boundary(x_pos, y_pos); //pass the new x,y position to function
- boundary(x_pos2, y_pos2); //pass the new x,y position to function
-     
- lcd.refresh();
- ThisThread::sleep_for(30ms);
+        boundary(x_pos, y_pos); //pass the new x,y position to function
+        boundary(x_pos2, y_pos2); //pass the new x,y position to function
+            
+        lcd.refresh();
+        ThisThread::sleep_for(30ms);
 
     }
 }
+
+
+void updateCharacterPosition(Joystick& joystick) {
+    Direction dir = joystick.get_direction();
+    if (dir == N) {
+        y_pos--;
+    } else if (dir == S) {
+        y_pos++;
+    } else if (dir == E) {
+        x_pos++;
+    } else if (dir == W) {
+        x_pos--;
+    } else if (dir == NE) {
+        y_pos--;
+        x_pos++;
+    } else if (dir == NW) {
+        y_pos--;
+        x_pos--;
+    } else if (dir == SE) {
+        y_pos++;
+        x_pos++;
+    } else if (dir == SW) {
+        y_pos++;
+        x_pos--;
+    }
+}
+
+void updateShootingDirection(Joystick& joystick) {
+    Direction dir = joystick.get_direction();
+    if (dir == N) {
+        y_pos2--;
+    } else if (dir == S) {
+        y_pos2++;
+    } else if (dir == E) {
+        x_pos2++;
+    } else if (dir == W) {
+        x_pos2--;
+    } else if (dir == NE) {
+        y_pos2--;
+        x_pos2++;
+    } else if (dir == NW) {
+        y_pos2--;
+        x_pos2--;
+    } else if (dir == SE) {
+        y_pos2++;
+        x_pos2++;
+    } else if (dir == SW) {
+        y_pos2++;
+        x_pos2--;
+    }
+}
+
 
 //check that the point remains on the screen
 void boundary(int x, int y){
